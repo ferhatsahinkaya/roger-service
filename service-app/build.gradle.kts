036852cd05ed1711e4fc0c50f4d4010e04be3e83
ferhatsahinkaya <ferhatsahinkaya@gmail.com>
2020-07-01 16:30:03 +0100

    Start using spring boot bootJar task to create fat jar instead of ShadowJar plugin

--- service-app/build.gradle.kts
+++ service-app/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     application
-    id("com.github.johnrengelman.shadow") version "4.0.3"
+    id("org.springframework.boot") version "2.3.1.RELEASE"
 }
 
 application {
     mainClassName = "com.roger.service.Launcher"
 }
 
-tasks {
-    withType(com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class.java) {
-        baseName = "service-fat"
-    }
-}
-
 dependencies {
     implementation("com.expediagroup:graphql-kotlin-spring-server:3.2.0")
+}
+
+tasks {
+    bootJar {
+        mainClassName = "com.roger.service.Launcher"
+    }
 }
