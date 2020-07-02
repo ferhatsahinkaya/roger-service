11a31410de52cbd23ea15d63515faf859130cdff
ferhatsahinkaya <ferhatsahinkaya@gmail.com>
2020-07-02 17:00:49 +0100

    Name operator link as operatorCode instead of code

--- service-app/src/main/kotlin/com/roger/service/domain/Service.kt
+++ service-app/src/main/kotlin/com/roger/service/domain/Service.kt
@@ -3,10 +3,10 @@ package com.roger.service.domain
 import com.expediagroup.graphql.federation.directives.FieldSet
 import com.expediagroup.graphql.federation.directives.KeyDirective
 
-@KeyDirective(fields = FieldSet("id code"))
+@KeyDirective(fields = FieldSet("id operatorCode"))
 data class Service(val id: Long,
                    val productType: String,
-                   val code: String)
+                   val operatorCode: String)
 
 // Needs to be different than Service class due to https://github.com/ExpediaGroup/graphql-kotlin/issues/363
 data class ServiceInput(val id: Long,
